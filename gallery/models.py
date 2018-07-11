656cf12f1e6322922d6323f786ed5676185e301a
Unknown <1925777036@qq.com>
2018-07-11 20:26:10 +0800

    7.11晚

--- gallery/models.py
+++ gallery/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Gallery(models.Model):
-    intro = models.CharField(max_length=100)
+    intro = models.CharField(default='描述', max_length=100)
+    img = models.ImageField(default='苦力怕.png', upload_to='static/images/')
+    title = models.CharField(default='标题', max_length=50)
+    def __str__(self):
+        return  self.title
