bd62a35f3fcfc6edbd02fdc3c63455b48caccbba
Unknown <1925777036@qq.com>
2018-07-11 14:17:10 +0800

    添加了源代码文件

---
+++ venv/00.py/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Administrator\Desktop\gitkraken\portfolio\venv\00.py\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
